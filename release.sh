58c9f8abbc10d9dd318ad4d375a5647dd13bad0b
cumberjie <26396320+cumberjie@users.noreply.github.com>
2022-06-28 16:56:57 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -116,10 +116,10 @@ function GenerateRules() {
                 "tls://dns.alidns.com"
             )
             foreign_dns=(
-             #   "https://hk-hkg.doh.sb/dns-query"
-             #   "https://doh.opendns.com/dns-query"
+                "https://hk-hkg.doh.sb/dns-query"
+                "https://doh.opendns.com/dns-query"
                 "tls://dns.google"
-             #   "https://8.8.8.8/dns-query"
+                "https://8.8.8.8/dns-query"
             )
             function GenerateRulesHeader() {
                 echo -n "[/" >> "${file_path}"
